d70a808a96a03f3a2301882a3eaccc785a9cc7e9
Christian Budtz <chrbudtz@gmail.com>
2021-03-10 20:45:56 +0100

    Intermediate fix

--- web/components/MarkDown.js
+++ web/components/MarkDown.js
@@ -4,7 +4,7 @@ import {getStrapiURL} from "../lib/api";
 
 
 export default function MarkDown({children}) {
-    const markdown = children.replace("(/uploads/", "("+ getStrapiURL() + "uploads/");
+    const markdown = children.replaceAll && children.replaceAll("(/uploads/", "("+ getStrapiURL() + "uploads/");
     return(
         <ReactMarkdown>
             {markdown}
--- web/pages/tiludlejere.js
+++ web/pages/tiludlejere.js
@@ -6,6 +6,7 @@ import TopBar from "../components/TopBar";
 import ReactMarkdown from "react-markdown";
 import {getStrapiMedia} from "../lib/media";
 import Footer from "../components/Footer";
+import MarkDown from "../components/MarkDown";
 
 function Tiludlejere({properties, pagematerials,pagemeta}) {
     const tiludlejere = pagematerials?.find((mat)=>mat.key==="tiludlejere");
@@ -43,39 +44,43 @@ function Tiludlejere({properties, pagematerials,pagemeta}) {
                 <Container>
                     <Row>
                         <Col style={{textAlign:"center"}}>
-                            <ReactMarkdown>
-                                {tiludlejere?.text}
-                            </ReactMarkdown>
+                            {/*<MarkDown>*/}
+                            {/*    {tiludlejere?.text}*/}
+                            {/*</MarkDown>*/}
                         </Col>
                     </Row>
+                    <Row style={{padding: 20}}/>
                     <Row>
                         {tiludlejere2?.images.map((image, key)=>
                             (<Col key={key} sm={4} style={{textAlign:"center"}}>
                                 <img style={{maxWidth:150,maxHeight:150, borderRadius:"50%"}} src={getStrapiMedia(image)}/>
-                                <p><h4>{tiludlejere2?.text.split(",")[key]}</h4></p>
+                                <h5><p>{tiludlejere2?.text.split(",")[key]}</p></h5>
                             </Col>)
 
                         )}
                     </Row>
                     <hr/>
+                    <Row><Col><h3 style={{textAlign:"center"}}>Har du eller kender du nogen med et godt hjem til vores gode udvalgte lejere?</h3></Col></Row>
                     <Row>
-                        <Col md={8}>
-                            <ReactMarkdown>
-                                {tiludlejere3?.text}
-                            </ReactMarkdown>
-                        </Col>
+                        <Col md={4}><h6><b>Gratis og uforpligtende besigtelse og vurdering</b></h6></Col>
                         <Col md={4}>
                             <img style={{maxWidth:"100%"}} src={getStrapiMedia(tiludlejere3?.images[0].formats.small)}/>
-
+                            <h6 style={{textAlign:"center",marginTop:15}}><b>Arne Backlund & Sara Heiberg</b></h6>
+                            <h6><i style={{textAlign:"center"}}>- Skal vi også udleje dit hjem?</i></h6>
                         </Col>
+                        <Col md={4}><h6><b>Udlejet eller gratis. Lige nu - frit tilsyn i udlejningsperioden</b></h6></Col>
                     </Row>
+                    <Row><Col><hr/></Col></Row>
                     <Row>
                         <Col>
-                            <ReactMarkdown>
-                                {tiludlejere4?.text}
-                            </ReactMarkdown>
+                            { tiludlejere4 &&
+                                <MarkDown>
+                                    {tiludlejere4?.text}
+                                </MarkDown>
+                            }
                         </Col>
                     </Row>
+                    <Row><Col><hr/></Col></Row>
                     <Row>
                         <img style={{maxWidth:"100%"}} src={getStrapiMedia(tiludlejere4?.images[0])}/>
                     </Row>
