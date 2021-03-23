a4ee43171284bf431ed2603ae6ad287ed780df97
Christian Budtz <chrbudtz@gmail.com>
2021-03-23 10:21:54 +0100

    improving component loadspeed

--- web/components/W1ImgW2Text.js
+++ web/components/W1ImgW2Text.js
@@ -6,14 +6,15 @@ import {resolveImage, useWindow} from "../lib/window";
 
 export default function W1ImgW2Text({element}){
     const [windowWidth, setWindowWidth] = useState(2000);
-    useWindow(setWindowWidth)
+    useWindow(setWindowWidth);
+    const colWidth = windowWidth >= 992 ? 349: (windowWidth > 768 ? windowWidth/2:windowWidth) ;
     return <Container>
         <Row>
             {element.imageplace==="right" &&
             <Col lg={8} md={6}><MarkDown>{element?.text}</MarkDown></Col>
             }
             <Col lg={4} md={6}>
-                <img width={"100%"} alt={element.image.name} src={getStrapiMedia(resolveImage(windowWidth,element?.image))}/>
+                <img width={"100%"} alt={element.image.name} src={getStrapiMedia(resolveImage(colWidth,element?.image))}/>
             </Col>
             {element.imageplace !== "right" &&
             <Col lg={8} md={6}><MarkDown>{element?.text}</MarkDown></Col>
